bfa23578aeba2aac63c546b8d39b941a78bf701c
yangtau <yanggtau@gmail.com>
2022-06-21 02:14:32 +0800

    Add support for Chinese

--- whoarder/clippings.py
+++ whoarder/clippings.py
@@ -1,11 +1,16 @@
-import chardet
+import argparse
 import codecs
 import os
+from typing import Dict, List
+
+import chardet
 import re
+from jinja2 import Environment, FileSystemLoader  # available from pip
+
+DEBUG = False
 
 
 class Clippings(object):
-
     def __init__(self, source, dest=None):
         '''
         Prepares the import and store it into the 'clippings' dict.
@@ -42,10 +47,7 @@ class Clippings(object):
         '''
         Output the clippings dict to HTML, using a Jinja2 template
         '''
-        from jinja2 import Environment, PackageLoader  # available from pip
-        env = Environment(loader=PackageLoader('whoarder', 'templates'),
-                          autoescape=True,
-                          extensions=['jinja2.ext.autoescape'])
+        env = Environment(loader=FileSystemLoader('./'))
         template = env.get_template('template1.html')
         render = template.render(clippings=self.clippings,
                                  book_author_couples=self.book_author_couples)
@@ -70,14 +72,25 @@ class ClippingsIterator(object):
 
     <contents>
     ==========
+
+    Chinese Example:
+    我的职业是小说家 (村上春树)
+    - 您在位置 #117-118的标注 | 添加于 2018年8月5日星期日 上午8:06:49
+
+    写小说这份活计，概而言之，实在是效率低下的营生。这是一种再三重复“比如说”的作业。
     '''
 
-    _clipping_separator = '==========\n'
     _clipping_line1 = re.compile(r'''
-        ^(?P<book>.*?)                           # Le Petit Prince
-        (\ \((?P<author>[^()]*)\))?$             #  (De Saint-Exupery, Antoine)
+        ^(?P<book>.*?)                                  # Le Petit Prince
+        (\ \((\[.*\])?(?P<author>[^()]*?)\ ?(\(.*\))?\))?$  # ([日]村上春树 (Haruki Murakami)) 
         ''', re.VERBOSE | re.IGNORECASE)
-    _clipping_line2 = re.compile(r'''
+
+    _clipping_book = re.compile(r'''
+            ^(.*?)                               # book
+            (【.*】|\ \(.*\)|（.*）)*$           # intro
+            ''', re.VERBOSE)
+
+    _clipping_line2_english = re.compile(r'''
         ^-\ (?:Your\ )?(?P<type>\w*)                         # Your Highlight
         (\ (?:on\ )?(?P<page>Unnumbered\ Page|Page\ .*)      #  on Page 42 |
         \ \|)?
@@ -86,86 +99,118 @@ class ClippingsIterator(object):
         \ \|\ Added\ on\ (?P<date>.*)$                       #  | Added on...
         ''', re.VERBOSE | re.IGNORECASE)
 
+    # - 您在第 1656 页（位置 #18097-18097）的标注 | 添加于 2022年1月9日星期日 下午11:11:58
+    _clipping_line2_chinese = re.compile(r'''
+        ^-\ (您在)(第\ (?P<page>\w*)\ 页)?(（?位置\ \#(?P<location>.*?)）?)?                         
+        的(?P<type>(标注|笔记))
+        \ \|\ 添加于\ (?P<date>.*)$                          #  | 添加于 ...
+        ''', re.VERBOSE | re.IGNORECASE)
+
+    _delimiter = '=========='
+
+    # ^-\ (您在)(第\ (?P<page>)\ 页)?(（?位置\ \#(?P<location>)）?)?的(?P<type>(标注|笔记))\ \|\ 添加于\ (?P<date>.*)$
+
     def __init__(self, source):
-        detected_encoding = _detect_encoding(source)
-        self.source_file = open(source, mode='r', encoding=detected_encoding)
+        self.lines = readlines(source)
+        self.cursor = 0
 
     def __iter__(self):
         return self
 
-    def __next__(self):
-        clipping_buffer = []
-        count = 1
-        while True:
-            if count > 5:
-                line_dict = self._clipping_line1.search(clipping_buffer[0])
-                line_dic2 = self._clipping_line2.search(clipping_buffer[1])
-                hl_type = None
-                if line_dic2:
-                    hl_type = line_dic2.groupdict()['type']
+    def __next__(self) -> Dict[str, str]:
+        if self.cursor >= len(self.lines):
+            raise StopIteration
 
-                if not line_dict or not line_dic2 or hl_type not in ('Note', 'Highlight'):
-                    raise InvalidFormatException(
-                        '''Input file doesn't seem to be
-                        a clippings file, got invalid header lines'''
-                    )
+        start = self.cursor
+        self.cursor = self.lines.index(self._delimiter, start)
+        if start == self.cursor or self.cursor == len(self.lines):
+            raise StopIteration
 
-                # Check for corruption - if the current line is a valid first
-                # line, then the content somehow got merged together.
-                if self._clipping_line1.search(clipping_buffer[-2]) and self._clipping_line2.search(clipping_buffer[-1]):
-                    raise InvalidFormatException(
-                        '''Input file doesn't seem to be
-                        a clippings file, separators are missing or damaged'''
-                    )
-
-            if self.source_file.closed:
-                raise StopIteration
-
-            line = self.source_file.readline()
-            if not line:
-                self.source_file.close()
-                raise StopIteration
-            elif line != self._clipping_separator:
-                # Kindle writes a FEFF BOM at the start of each clipping
-                # (i.e. every 6 lines), which is clearly wrong. We strip it.
-                if line[0] == "\ufeff":
-                    line = line.replace("\ufeff", "")
-                clipping_buffer.append(line.strip())
-                count += 1
-            else:
-                break
-
-        if 'Page' not in clipping_buffer[1] and 'page' not in clipping_buffer[1]:
-            clipping_buffer[1] = re.sub(r'(- Your .*?) (.*)',
-                                        r'\1 on Unnumbered Page | \2',
-                                        clipping_buffer[1])
-
+        cur = self.lines[start:self.cursor]
+        self.cursor += 1
         try:
-            line_dict = self._clipping_line1.search(clipping_buffer[0]).groupdict()
-            line_dic2 = self._clipping_line2.search(clipping_buffer[1]).groupdict()
-            line_dict.update(line_dic2)
-            line_dict['contents'] = '\n'.join(clipping_buffer[3:]).strip()
-            return line_dict
-        except AttributeError:
-            print("Failed to import the following note, please report to https://github.com/ronjouch/whoarder :\n  {0}\n".format(clipping_buffer))
-            return self.__next__()
+            return self.__parse(cur)
+        except Exception as e:
+            raise InvalidFormatException(
+                "Exception: {}\nFailed to parse: {}".format(e, cur))
+
+    def __parse(self, content: List[str]) -> Dict[str, str]:
+        '''
+        Parses the content of a clipping and returns a dict with the
+        '''
+        if len(content) < 4:
+            raise ValueError(
+                'Invalid clipping, less than 4 lines:\n{}'.format(content))
+
+        # 1. Parse the first line
+        result = unwrap(self._clipping_line1.search(content[0])).groupdict()
+        book = result['book']
+        if book:
+            result['book'], _ = unwrap(
+                self._clipping_book.match(book.strip())).groups()
+
+        # 2. Parse the second line
+        zh = self._clipping_line2_chinese.search(content[1])
+        if zh:
+            dict = zh.groupdict()
+            dict['type'] = 'Highlight' if dict['type'] == '标注' else 'Note'
+            dict['page'] = 'page ' + dict['page'] if dict['page'] else None
+        else:
+            dict = unwrap(
+                self._clipping_line2_english.search(content[1])).groupdict()
+        result.update(dict)
+
+        # 3. Parse the content
+        assert content[2] == '', 'Invalid clipping, line 3 is not empty'
+        result["contents"] = '\n'.join(content[3:]).strip()
+
+        if DEBUG:
+            print(content)
+            print(result)
+        return result
 
 
-def _detect_encoding(source):
+def unwrap(result: None | re.Match[str]) -> re.Match[str]:
+    match result:
+        case None:
+            raise Exception('No result')
+        case _:
+            return result
+
+
+def readlines(source) -> List[str]:
     '''
     Returns the encoding of the source file, using chardet.
     '''
-    rawdata = open(source, "rb").read()
-    # chardet detects UTF-8 with BOM as 'UTF-8' (I don't know why), i.e.
-    # fails to notify us about the BOM, resulting in a string prepended
-    # with \ufeff, so we manually detect and set the utf-8-sig encoding
-    if rawdata.startswith(codecs.BOM_UTF8):
-        detected_encoding = 'utf-8-sig'
-    else:
-        result = chardet.detect(rawdata)
-        detected_encoding = result['encoding']
-    return detected_encoding
+    with open(source, "rb") as f:
+        rawdata = f.read()
+        # chardet detects UTF-8 with BOM as 'UTF-8' (I don't know why), i.e.
+        # fails to notify us about the BOM, resulting in a string prepended
+        # with \ufeff, so we manually detect and set the utf-8-sig encoding
+        if rawdata.startswith(codecs.BOM_UTF8):
+            detected_encoding = 'utf-8-sig'
+        else:
+            result = chardet.detect(rawdata)
+            detected_encoding = result['encoding']
+        return codecs.decode(rawdata, detected_encoding).splitlines()
 
 
 class InvalidFormatException(BaseException):
     pass
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="whoarder converts Kindle \
+                        'My Clippings.txt' files to more pleasant HTML.")
+    parser.add_argument('source',
+                        help='Path to the source file, stored by Kindle in \
+                        /Media/Kindle/documents/My Clippings.txt.')
+    parser.add_argument('destination',
+                        help='Target HTML file. If omitted, a .html bearing \
+                        the same name as the input .txt file will be used.',
+                        nargs='?', default=None)
+    args = parser.parse_args()
+
+    clippings = Clippings(args.source, args.destination)
+    clippings.export_to_html()
+    print('Successfully wrote ' + clippings.dest + "\n")
